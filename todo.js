394f689d960c61d41b7249eaff8fb8ec89eae4c2
Taiwo Adepoju <tadepoju95@gmail.com>
2020-11-05 01:39:03 -0600

    Modified js file in project folder.

--- todo.js
+++ todo.js
@@ -1,78 +1,63 @@
 
 let fetchTodos = () => {
-    let getValues = JSON.parse(localStorage.getItem('userInput'));
+    let getValues = JSON.parse(localStorage.getItem('todoArray'));
     if(getValues === null) {
+       getValues = []
 	   axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
 		 .then(response => {
 		 	let todos = response.data;
-		 	console.log(`GET todos`, todos); 
-           	let listTitles = document.getElementsByClassName('user-value')[0];
-            let createElement = '<ul>'
            for(let i = 0; i < todos.length; i++) {
-              createElement += '<li>' + todos[i].title + '</li>';
+           	getValues.push(todos[i].title);
            }
-           createElement += '</ul>'
-           listTitles.innerHTML = createElement;
+           	localStorage.setItem('todoArray', JSON.stringify(getValues));
+           	listValues();
 		 })
 		 .catch(error => console.error(error));  
     }
-    console.log(getValues);
 }
 fetchTodos();
 
 
 let setValues = () => {
 	let userInput = document.getElementsByClassName('form-controls')[0].value;
-	let getValues = JSON.parse(localStorage.getItem('userInput'));
-	if(getValues === null) {
-		getValues = [];
-	}
-	console.log(getValues);
+	let getValues = JSON.parse(localStorage.getItem('todoArray'));
 	getValues.push(userInput);
-	localStorage.setItem('userInput', JSON.stringify(getValues))
+	localStorage.setItem('todoArray', JSON.stringify(getValues)) 
 	document.getElementsByClassName('form-controls')[0].value = '';
 	listValues();
 }
 
 
 let listValues = () => {
-	let listItems = document.getElementsByClassName('user-value')[0];
-	let getValues = JSON.parse(localStorage.getItem('userInput'));
-	console.log(getValues);
-	let listingItems = '';
+	let divElement = document.getElementsByClassName('user-value')[0];
+	let getValues = JSON.parse(localStorage.getItem('todoArray'));
+	let createElements = '';
 	for(let i = 0; i < getValues.length; i++) {
-		listingItems += "<br><input value='" + getValues[i] + "' " + "id='" + i + "'" + "readonly/>" + "<button onclick='edit("+ i + ")' id='editItems'>" + "edit" + "</button>" + "<button onclick='update("+ i + ")' id='updateItems'>" + "update"  + "</button>" + "<button onclick='deleteItems("+ i + ")' id='deleteItems'>" + "delete"  + "</button>";
+		createElements += "<br><input value='" + getValues[i] + "' " + "id='" + i + "'" + "readonly/>" + "<button onclick='edit("+ i + ")' id='editItems'>" + "edit" + "</button>" + "<button onclick='update("+ i + ")' id='updateItems'>" + "update"  + "</button>" + "<button onclick='deleteItems("+ i + ")' id='deleteItems'>" + "delete"  + "</button>";
 	}
-	 listItems.innerHTML = listingItems;
+	 divElement.innerHTML = createElements;
 }
 
+
 let edit = (id) => {
-	console.log(id)
-	//let removeAtrribute = document.getElementById('id').disabled = false;
 	let disableReadOnly = document.getElementById(id).readOnly = false;
 	let inputFocus = document.getElementById(id).focus();
 }
 
+
 let update = (id) => {
 	let updateTodoItem = document.getElementById(id).value;
-	let getValues = JSON.parse(localStorage.getItem('userInput'));
+	let getValues = JSON.parse(localStorage.getItem('todoArray'));
 	getValues.splice(id, 1, updateTodoItem);
-	localStorage.setItem('userInput', JSON.stringify(getValues));
+	localStorage.setItem('todoArray', JSON.stringify(getValues));
 
 }
 
 
 let deleteItems = (id) => {
-	let getValues = JSON.parse(localStorage.getItem('userInput'));
-	getValues.splice(id, 1);
-	localStorage.setItem('userInput', JSON.stringify(getValues));
-	let deleteInputElement = document.getElementById(id);
-	deleteInputElement.parentNode.removeChild(deleteInputElement); 
-	let deleteEditButton = document.getElementById('editItems');
-	deleteEditButton.parentNode.removeChild(deleteEditButton);
-	let deleteUpdateButton = document.getElementById('updateItems');
-	deleteUpdateButton.parentNode.removeChild(deleteUpdateButton);
-	let deleteButton = document.getElementById('deleteItems');
-	deleteButton.parentNode.removeChild(deleteButton);
+	let getValues = JSON.parse(localStorage.getItem('todoArray'));
+	getValues.splice(id, 1); 
+	localStorage.setItem('todoArray', JSON.stringify(getValues));
+	listValues();
 }
 
