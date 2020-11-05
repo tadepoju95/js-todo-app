749e43f40724ce56f8a8d25a7876a02a672bba0f
Taiwo Adepoju <tadepoju95@gmail.com>
2020-11-05 02:09:34 -0600

    Modified js file in project folder.

--- todo.js
+++ todo.js
@@ -5,7 +5,6 @@ let setValues = () => {
 	if(getValues === null) {
 		getValues = [];
 	}
-	console.log(getValues);
 	getValues.push(userInput);
 	localStorage.setItem('userInput', JSON.stringify(getValues))
 	document.getElementsByClassName('form-controls')[0].value = '';
@@ -15,7 +14,6 @@ let setValues = () => {
 let listValues = () => {
 	let listItems = document.getElementsByClassName('user-value')[0];
 	let getValues = JSON.parse(localStorage.getItem('userInput'));
-	console.log(getValues);
 	let listingItems = '';
 	for(let i = 0; i < getValues.length; i++) {
 		if(localStorage.getItem('userInput') !== null) {
@@ -26,8 +24,6 @@ let listValues = () => {
 }
 
 let edit = (id) => {
-	console.log(id)
-	//let removeAtrribute = document.getElementById('id').disabled = false;
 	let disableReadOnly = document.getElementById(id).readOnly = false;
 	let inputFocus = document.getElementById(id).focus();
 }
@@ -45,14 +41,7 @@ let deleteItems = (id) => {
 	let getValues = JSON.parse(localStorage.getItem('userInput'));
 	getValues.splice(id, 1);
 	localStorage.setItem('userInput', JSON.stringify(getValues));
-	let deleteInputElement = document.getElementById(id);
-	deleteInputElement.parentNode.removeChild(deleteInputElement); 
-	let deleteEditButton = document.getElementById('editItems');
-	deleteEditButton.parentNode.removeChild(deleteEditButton);
-	let deleteUpdateButton = document.getElementById('updateItems');
-	deleteUpdateButton.parentNode.removeChild(deleteUpdateButton);
-	let deleteButton = document.getElementById('deleteItems');
-	deleteButton.parentNode.removeChild(deleteButton);
+	listValues();
 }
 
 
